1f9fa53523dc2474e2d5cdb3a1007f79c20d8c49
230493131013mihir <230493131013.mihir@gmail.com>
2026-03-17 20:12:13 +0530

    bv

--- frontend/src/component/Btn2/Btn2.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Btn2(props) {
-  return <button variant="outlined">Outlined</button>;
-}
-
-export default Btn2;
--- frontend/src/container/About/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function About(props) {
-    return (
-        <h1>hello dental</h1>
-    );
-}
-
-export default About;
