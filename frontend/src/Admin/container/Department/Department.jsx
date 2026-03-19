1bacef7b4d6d375c083917d02f5781ddcf14bfcd
230493131013mihir <230493131013.mihir@gmail.com>
2026-03-19 21:19:21 +0530

    home

--- frontend/src/Admin/container/Department/Department.jsx
+++ frontend/src/Admin/container/Department/Department.jsx
@@ -18,6 +18,7 @@ import IconButton from '@mui/material/IconButton';
 
 import DeleteIcon from '@mui/icons-material/Delete';
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import { update } from "../../redux/slice/departmentSlice";
 
 
 function Department(props) {
